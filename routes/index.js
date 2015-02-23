049f80f53a03483161c2d370a3206bdfae4c4d73
Wyatt Johnson <wyattjoh@gmail.com>
2015-02-22 19:43:56 -0700

    Initial commit

---
+++ routes/index.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Dylan Hanwell for VP EXTERNAL' });
+});
+
+module.exports = router;

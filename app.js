6e14f07d94a4939e825a89dcaf53d94fa91efa35
Wyatt Johnson <wyattjoh@gmail.com>
2015-02-23 00:22:17 -0700

    Added favicons

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var favicon = require('serve-favicon');
 
 var session = require('express-session');
 var memjs = require('memjs');
@@ -54,6 +55,7 @@ app.use(csrf());
 
 app.use(require('less-middleware')(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(favicon(path.join(__dirname,'public','images','favicon.ico')));
 
 app.use('/', routes);
 app.use('/send', send);

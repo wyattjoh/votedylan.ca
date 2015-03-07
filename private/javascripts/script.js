425568d0744dafafa99ec3c3adba56aa3bf76ef9
Wyatt Johnson <wyattjoh@gmail.com>
2015-03-07 16:35:01 -0700

    Added FWD text

--- private/javascripts/script.js
+++ private/javascripts/script.js
@@ -47,16 +47,38 @@ $(document).ready(function() {
     });
   });
 
-  $("#fwd").click(function(e) {
-    e.preventDefault();
-    $(document.body).html("<div id=\"overlay\"></div><iframe id=\"video\" src=\"//www.youtube.com/embed/vSUW-Z_Cnc0?rel=0&autoplay=1&controls=0&showinfo=0&loop=1&playlist=vSUW-Z_Cnc0\" frameborder=\"0\" allowfullscreen></iframe>")
+  var playVideo = function(movieID, name, stopText) {
+    $(document.body).html("<div id=\"overlay\"></div><iframe id=\"video\" src=\"//www.youtube.com/embed/" + movieID + "?rel=0&autoplay=1&controls=0&showinfo=0&loop=1&playlist=" + movieID + "\" frameborder=\"0\" allowfullscreen></iframe>")
     $("#video").css({ width: $(window).width() + 'px', height: $(window).height() + 'px' });
-    ga('send', 'event', 'bearforce', 'click', 'FWD', 1);
+    ga('send', 'event', name, 'click', 'FWD', 1);
     window.onbeforeunload = function () {
-            return "But... Bear...Force...One....";
+            return stopText;
     }
     function prevent() {
         window.onbeforeunload = function () { };
     };
+  };
+
+  var playYoutube = function() {
+    return playVideo("vSUW-Z_Cnc0", "bearforce", "But... Bear...Force...One....");
+  };
+
+  var charQueue = [];
+
+  $(document).keypress(function(e) {
+    if (charQueue.length == 0 && e.which == 102) {
+      charQueue.push(e.which);
+    } else if (charQueue.length == 1 && e.which == 119) {
+      charQueue.push(e.which);
+    } else if (charQueue.length == 2 && e.which == 100) {
+      playYoutube();
+    } else {
+      charQueue = [];
+    }
+  });
+
+  $("#fwd").click(function(e) {
+    e.preventDefault();
+    playYoutube();
   });
 });

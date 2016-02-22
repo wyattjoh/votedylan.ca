1dfada36bdd18037ffd5b9a1e133e8206187e9e4
Wyatt Johnson <wyattjoh@gmail.com>
2016-02-22 10:52:43 -0700

    Removed pass

--- public/javascripts/pass.js
+++
@@ -1,8 +0,0 @@
-function showPage(){
-    if(d3.select("#usName").node().value =="admin"&&
-        d3.select("#pass").node().value =="UA1908"
-    ){
-        d3.select("#fakeMain").style("display","none");
-        d3.select(".main").style("display","block");
-    }
-}
